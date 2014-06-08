d2cc537a9040d6abfdbf1479aeaef7a47d23cf2a
Bill Brown <Mixon851@gmail.com>
2014-06-08 15:04:23 -0400

    Added more journalservice endpoints

--- Systematize.ServiceInterface/JournalService.cs
+++ Systematize.ServiceInterface/JournalService.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using Systematize.ServiceModel.Types;
 using ServiceStack;
 using ServiceStack.Data;
@@ -21,12 +24,87 @@ namespace Systematize.ServiceInterface
 
         public object Get(Journals request)
         {
-            var sw = Stopwatch.StartNew();
             using (var db = _connectionFactory.Open())
             {
                 var journals = db.Select<Journal>();
                 return journals;
             }
+        }
+
+        public object Get(GetJournal message)
+        {
+            using (var db = _connectionFactory.Open())
+            {
+                var journal = db.SelectByIds<Journal>(new long[] {message.Id}).FirstOrDefault();
+                if (journal == null)
+                    return new HttpResult() {StatusCode = HttpStatusCode.NotFound};
+
+                return journal;
+            }
+        }
+
+        public object Post(CreateJournal message)
+        {
+            var sw = Stopwatch.StartNew();
+            using (var db = _connectionFactory.Open())
+            {
+                var journal = new Journal
+                {
+                    Name = message.Name,
+                    Description = message.Description,
+                    CreatedAt = DateTime.Now
+                };
+
+                db.Insert(journal);
+            }
+            sw.Stop();
+            return new JournalResponse {TimeTakenMs = sw.ElapsedMilliseconds};
+        }
+
+        public object Put(UpdateJournal message)
+        {
+            var sw = Stopwatch.StartNew();
+            using (var db = _connectionFactory.Open())
+            {
+                var journal = db.SelectByIds<Journal>(new long[] {message.Id}).FirstOrDefault();
+                if (journal == null)
+                    return new HttpResult() {StatusCode = HttpStatusCode.NotFound};
+
+                try
+                {
+                    journal.Name = message.Name;
+                    journal.Description = message.Description;
+                    db.Update(journal);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            sw.Stop();
+            return new JournalResponse {TimeTakenMs = sw.ElapsedMilliseconds};
+        }
+
+        public object Delete(DeleteJournal message)
+        {
+            var sw = Stopwatch.StartNew();
+            using (var db = _connectionFactory.Open())
+            {
+                var journal = db.SelectByIds<Journal>(new long[] {message.Id});
+                if (journal == null)
+                    return new HttpResult() {StatusCode = HttpStatusCode.NotFound};
+
+                try
+                {
+                    db.Delete(journal);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            sw.Stop();
+            return new JournalResponse {TimeTakenMs = sw.ElapsedMilliseconds};
         }
     }
 }
